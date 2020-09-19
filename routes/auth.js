13ba6df3317a24f083d34c92b8b04d50ef6f15d4
Basliel <bababayehu22@wooster.edu>
2020-09-18 23:59:51 -0400

    add middleware and protecting routes

--- routes/auth.js
+++ routes/auth.js
@@ -9,13 +9,21 @@ const { genSalt } = require("bcryptjs");
 
 const jwt = require("jsonwebtoken");
 const config = require("config");
+
+const auth = require("../middleware/auth");
 const bcrypt = require("bcryptjs");
 
 //@route    GET api/auth
 //@desc     get logged in user
 //@access   private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 //@route    POST api/auth
--- routes/playlists.js
+++ routes/playlists.js
@@ -1,19 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
+const Playlist = require("../models/Playlist");
 
 //@route    GET api/playlist
 //@desc     Get all Plalists
 //@access   private
-router.get("/", (req, res) => {
-  res.send("Get all Plalists");
+router.get("/", auth, async (req, res) => {
+  try {
+    const playlists = await Playlist.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(playlists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 //@route    POST api/playlist
 //@desc     add new Plalists
 //@access   private
-router.post("/", (req, res) => {
-  res.send("add new Plalist");
-});
+router.post(
+  "/",
+  [auth, check("name", "Name is required").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newPlaylist = new Playlist({
+        name,
+        date,
+        user: req.user.id,
+      });
+      const playlist = await newPlaylist.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 //@route    PUT api/playlist/:id
 //@desc     update playlist
